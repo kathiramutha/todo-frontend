76112a8c6fc43fca8d8edd0a2c9a62c08786736a
kathiramutha <kathiramuthacse14@gmail.com>
2026-03-16 11:19:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,56 @@
-const API_URL = "http://Todo-backend-env.eba-zv3zrdie.ap-south-1.elasticbeanstalk.com/tasks";
+const API_URL = "http://todo-backend-env.eba-zv3zrdie.ap-south-1.elasticbeanstalk.com/tasks";
 
 function fetchTasks() {
+
 fetch(API_URL)
+
 .then(response => response.json())
+
 .then(data => {
 
-```
-        console.log("Tasks:", data);
+const taskList=document.getElementById("taskList");
 
-        const taskList = document.getElementById("taskList");
-        taskList.innerHTML = "";
+taskList.innerHTML="";
 
-        data.forEach(task => {
-            const li = document.createElement("li");
-            li.textContent = task.task;
-            taskList.appendChild(li);
-        });
+data.forEach(task => {
 
-    })
-    .catch(error => console.log("Fetch Error:", error));
-```
+const li=document.createElement("li");
+
+li.textContent=task.task;
+
+taskList.appendChild(li);
+
+});
+
+});
 
 }
 
-function addTask() {
+function addTask(){
 
-```
-const taskInput = document.getElementById("taskInput").value;
+const taskInput=document.getElementById("taskInput").value;
 
-fetch(API_URL, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ task: taskInput })
+fetch(API_URL,{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({task:taskInput})
+
 })
-.then(response => response.json())
-.then(data => {
-    console.log("Added:", data);
-    fetchTasks();
-    document.getElementById("taskInput").value = "";
-})
-.catch(error => console.log("POST Error:", error));
-```
+
+.then(response=>response.json())
+
+.then(data=>{
+
+fetchTasks();
+
+document.getElementById("taskInput").value="";
+
+});
 
 }
 
